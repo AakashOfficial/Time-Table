14bb1de17888798898a144dcb8ffb7d80f08a00a
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-04-02 00:14:38 +0530

    Poject Configuration Done !

---
+++ Term Paper/src/com/tyagi/model/MyData.java
@@ -0,0 +1,29 @@
+package com.tyagi.model;
+
+import java.io.Serializable;
+
+public class MyData implements Serializable {
+
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String toString() {
+		return "MyData[ username=" + username + ",password=" + password + "]";
+	}
+}
