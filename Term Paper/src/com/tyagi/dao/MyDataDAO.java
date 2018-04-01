e02c18c1786ba25fd6df743256288592ebdcc75d
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-04-02 00:16:39 +0530

    Project Configuration Done !

---
+++ Term Paper/src/com/tyagi/dao/MyDataDAO.java
@@ -0,0 +1,11 @@
+package com.tyagi.dao;
+
+import com.tyagi.model.MyData;
+import java.util.List;
+
+public interface MyDataDAO {
+
+  public boolean addData(MyData m);
+  public boolean Login(MyData m);
+  //public boolean deleteuser(String username);
+}
